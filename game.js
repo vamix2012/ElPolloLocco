89b0b43aac1c4c709a48a8a7dc04f18823600f12
Lucian-Cristian Toma <vamix2012@gmail.com>
2020-11-26 00:38:26 +0100

    new enemies and final boss added and bottle throw function

--- game.js
+++ game.js
@@ -5,26 +5,95 @@ let isMovingRight = false;
 let isMovingLeft = false;
 let bg_elements = 0;
 let lastJumpStarted = 0;
+let character_energy = 100;
 let character_y = 250;
 let currentCharacterImage = 'img/charakter_1.png';
 let characterGraphicsRight = ['img/charakter_1.png', 'img/charakter_2.png', 'img/charakter_3.png', 'img/charakter_4.png'];
 let characterGraphicsLeft = ['img/charakter_left_1.png', 'img/charakter_left_2.png', 'img/charakter_left_3.png', 'img/charakter_left_4.png'];
 let characterGraphicsIndex = 0;
 let cloudOffset = 0;
+let chickens = [];
+let placedBottles = [1000, 1400, 1700, 2300, 2500, 2900, 3300, 3900];
+let colectedBottles = 10;
+let bottleThrowTime = 0;
 
 // ******************* Game config ****************** \\
 let JUMP_TIME = 250; //in ms 
 let GAME_SPEED = 5;
+let AUDIO_RUNNING = new Audio('audio/running.mp3');
+let AUDIO_JUMP = new Audio('audio/jump.mp3');
+let AUDIO_BOTTLE = new Audio('audio/bottle.mp3')
+
 function init() {
 
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
+    createChickenList();
     checkForRunning();
     draw();
     calculateCloudOffset();
     listenForKeys();
+    calculateChickenPosition();
+    checkForCollision();
+
 }
 
+function checkForCollision() {
+    setInterval(function () {
+        //Check chicken
+        for (let i = 0; i < chickens.length; i++) {
+            let chicken = chickens[i];
+            let chicken_x = chicken.position_x + bg_elements;
+            if ((chicken_x - 40) < character_x && (chicken_x + 40) > character_x) {
+                if (character_y > 210) {
+                    character_energy -= 5;
+                }
+                if (character_energy < 5) {
+                    character_energy = 0;
+                }
+
+            }
+        }
+
+        for (let i = 0; i < placedBottles.length; i++) {
+            let bottle_x = placedBottles[i] + bg_elements;
+            if ((bottle_x - 40) < character_x && (bottle_x + 40) > character_x) {
+                if (character_y > 210) {
+                    AUDIO_BOTTLE.play();
+                    placedBottles.splice(i, 1);
+                    colectedBottles++;
+                }
+            }
+        }
+    }, 200);
+}
+
+function calculateChickenPosition() {
+    setInterval(function () {
+        for (let i = 0; i < chickens.length; i++) {
+            let chicken = chickens[i];
+            chicken.position_x = chicken.position_x - chicken.speed;
+        }
+    }, 200);
+
+
+}
+function createChickenList() {
+    chickens = [
+
+        createChicken(2, 600),
+        createChicken(1, 1000),
+        createChicken(2, 1400),
+        createChicken(1, 1900),
+        createChicken(1, 2400),
+        createChicken(2, 2900),
+        createChicken(1, 3400),
+        createChicken(1, 4000)
+
+    ];
+
+
+}
 function calculateCloudOffset() {
     setInterval(function () {
         cloudOffset = cloudOffset + 0.25;
@@ -34,18 +103,24 @@ function calculateCloudOffset() {
 function checkForRunning() {
     setInterval(function () {
         if (isMovingRight) {
+            AUDIO_RUNNING.play();
             let index = characterGraphicsIndex % characterGraphicsRight.length;
             currentCharacterImage = characterGraphicsRight[index];
             characterGraphicsIndex++;
         }
 
         if (isMovingLeft) {
+            AUDIO_RUNNING.play();
             let index = characterGraphicsIndex % characterGraphicsLeft.length;
             currentCharacterImage = characterGraphicsLeft[index];
             characterGraphicsIndex++;
+        }
 
+        if (!isMovingLeft && !isMovingRight) {
+            AUDIO_RUNNING.pause();
         }
     }, 100);
+
 
 }
 
@@ -54,28 +129,74 @@ function draw() {
     drawBackground();
     updateCharacter();
     drawChicken();
+    drawBottles();
     requestAnimationFrame(draw);
+    drawEnergyBar();
+    drawInformation();
+    drawThrowBottle();
+    drawFinalBoss();
+
+}
+
+function drawFinalBoss() {
+    let chicken_x = 5000;
+    addBackgroundObject('img/chicken_big.png', chicken_x, 98, 0.45, 1);
+}
+
+function drawThrowBottle() {
+    if (bottleThrowTime) {
+        let timePassed = new Date().getTime() - bottleThrowTime;
+        let gravity = Math.pow(9.81, timePassed / 289)
+        let bottle_x = 220 + (timePassed * 0.9);
+        let bottle_y = 300 - (timePassed * 0.4 - gravity);
+
+        let base_image = new Image();
+        base_image.src = 'img/tabasco.png';
+        if (base_image.complete) {
+            ctx.drawImage(base_image, bottle_x, bottle_y, base_image.width * 0.5, base_image.height * 0.5);
+        };
+    }
 
 
 }
 
+function drawInformation() {
+    let base_image = new Image();
+    base_image.src = 'img/tabasco.png';
+    if (base_image.complete) {
+        ctx.drawImage(base_image, 0, 0, base_image.width * 0.5, base_image.height * 0.5);
+    };
+    ctx.globalAlpha = 1;
+
+    ctx.font = '26px Comic Sans MS';
+    ctx.fillText('x' + colectedBottles, 45, 33)
+}
+
+function drawBottles() {
+    for (let i = 0; i < placedBottles.length; i++) {
+        let bottle_x = placedBottles[i];
+        addBackgroundObject('img/tabasco.png', bottle_x, 335, 0.5, 1);
+    }
+}
+
+function drawEnergyBar() {
+    ctx.globalAlpha = 0.5;
+    ctx.fillStyle = "blue";
+    ctx.fillRect(500, 15, 2 * character_energy, 30);
+
+    ctx.globalAlpha = 0.2;
+    ctx.fillStyle = "black";
+    ctx.fillRect(495, 10, 210, 40);
+    ctx.globalAlpha = 1;
+}
+
 function drawChicken() {
-    let chickens = [
-        createChicken(1, 200),
-        createChicken(1, 400),
-        createChicken(2, 600),
-        createChicken(1, 700),
-        createChicken(2, 800),
-        createChicken(1, 900),
-        createChicken(1, 1000),
-        createChicken(2, 11000)
-    ];
-  
-        for (i = 0; i < chickens.length; i = i + 1) {
-            let chicken = chickens[i];
-            addBackgroundObject(chicken.img, chicken.position_x, chicken.position_y, chicken.scale, 1);
-        }
-       
+
+    for (i = 0; i < chickens.length; i = i + 1) {
+        let chicken = chickens[i];
+        addBackgroundObject(chicken.img, chicken.position_x, chicken.position_y, chicken.scale, 1);
+    }
+
 }
 
 function createChicken(type, position_x) {
@@ -83,7 +204,8 @@ function createChicken(type, position_x) {
         "img": "img/chicken" + type + ".png",
         "position_x": position_x,
         "position_y": 325,
-        "scale": 0.6
+        "scale": 0.6,
+        "speed": (Math.random() * 9)
     };
 }
 
@@ -129,7 +251,8 @@ function drawGround() {
     if (isMovingRight) {
         bg_elements = bg_elements - GAME_SPEED;
     }
-    if (isMovingLeft) {
+
+    if (isMovingLeft && bg_elements < 500) {
         bg_elements = bg_elements + GAME_SPEED;
     }
     addBackgroundObject('img/bg_elem_1.png', 0, 195, 0.6, 0.4);
@@ -182,15 +305,23 @@ function listenForKeys() {
         const k = e.key;
         if (k == 'ArrowRight') {
             isMovingRight = true;
-            //character_x = character_x + 5;
         }
         if (k == 'ArrowLeft') {
             isMovingLeft = true;
-            // character_x = character_x - 5;
+        }
+
+        if (k == 'd') {
+            let timePassed = new Date().getTime() - bottleThrowTime;
+            if (timePassed > 1000) {
+                colectedBottles--;
+                bottleThrowTime = new Date().getTime();
+            }
+
         }
 
         let timePassedSinceJump = new Date().getTime() - lastJumpStarted;
         if (k == ' ' && timePassedSinceJump > JUMP_TIME * 2) {
+            AUDIO_JUMP.play();
             lastJumpStarted = new Date().getTime();
         }
     });
@@ -198,15 +329,10 @@ function listenForKeys() {
         const k = e.key;
         if (k == 'ArrowRight') {
             isMovingRight = false;
-            //character_x = character_x + 5;
         }
         if (k == 'ArrowLeft') {
             isMovingLeft = false;
-            // character_x = character_x - 5;
         }
-        /*if (k == ' '){
-         isJumping = false;
 
-        }*/
     });
 }
